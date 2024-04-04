a130053449f99bcc9a11aa2d62bcf75208bd1e2f
katyastan <134015338+katyastan@users.noreply.github.com>
2024-04-05 01:51:40 +0300

    feat: 6 tasks passing

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -106,8 +106,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
@@ -122,8 +122,9 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const resolvedPromises = promises.map((promise) => promise.catch(() => null));
+  return Promise.all(resolvedPromises);
 }
 
 /**
